dd5066742d008ffc819b047d7f7ef3b926718c54
Bohdan Shraier <bohdanshraier@Bohdans-MacBook-Air.local>
2025-04-07 05:13:30 +0300

    compeleted header ui

--- src/components/Root/Header/Header.tsx
+++ src/components/Root/Header/Header.tsx
@@ -1,19 +1,43 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import arrow from '@images/back-arrow.png';
+import heart from '@images/heart.png';
 
 const Header = () => {
 	const insets = useSafeAreaInsets();
 	const paddingTop = insets.top;
 	return (
-		<View style={[{ height: paddingTop + 50 }]}>
+		<View style={[{ height: paddingTop + 60 }]}>
 			<LinearGradient
 				colors={['#43BCF0', '#4E5DB2', '#4F54AC', '#571280']}
 				locations={[0, 0.56, 0.61, 1]}
 				start={{ x: 0, y: 0 }}
 				end={{ x: 0, y: 1 }}
-				style={styles.header}
-			></LinearGradient>
+				style={styles.layout}
+			>
+				<View style={[styles.body, { paddingTop }]}>
+					<TouchableOpacity>
+						<Image style={styles.header_item} resizeMode='contain' source={arrow} />
+					</TouchableOpacity>
+					<TouchableOpacity>
+						<Image style={styles.header_item} resizeMode='contain' source={heart} />
+					</TouchableOpacity>
+
+					<TouchableOpacity>
+						<LinearGradient
+							colors={['#00FFB2', '#24BFC9']}
+							locations={[0, 0.56, 0.61, 1]}
+							start={{ x: 0, y: 0 }}
+							end={{ x: 0, y: 1 }}
+							style={[styles.header_item, styles.points]}
+						>
+							<Text style={styles.points_text}>2/8</Text>
+						</LinearGradient>
+					</TouchableOpacity>
+				</View>
+			</LinearGradient>
 		</View>
 	);
 };
@@ -21,7 +45,30 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
-	header: {
+	layout: {
 		flex: 1,
+	},
+	body: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingHorizontal: 15,
+	},
+	header_item: {
+		width: 42,
+		height: 35,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	points: {
+		height: 35,
+		width: 50,
+		paddingHorizontal: 10,
+		borderRadius: 15,
+	},
+
+	points_text: {
+		color: '#fff',
 	},
 });
